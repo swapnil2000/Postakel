e7e6b0c6a29a10a1d80e9430a54fca17a273fe23
swapnil2000 <55456743+swapnil2000@users.noreply.github.com>
2025-10-22 20:26:52 +0530

    Delete Eat With Me POS directory

--- Eat With Me POS/server/src/services/vendorService.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from '../app';
-
-export async function getVendorWithRestaurant(vendorId: string) {
-    return prisma.vendor.findUnique({ where: { id: vendorId }, include: { restaurant: true } });
-    
-}
