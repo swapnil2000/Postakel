6a563622db5bbb787dd854aac4be6602252868d4
swapnil2000 <swapnilsharma0220@gmail.com>
2025-10-28 03:22:46 +0530

    Categories Management Working, Menu Management, Staff adding working

--- Eat With Me POS new/backend/src/controllers/menu.ts
+++ Eat With Me POS new/backend/src/controllers/menu.ts
@@ -54,22 +54,23 @@ export async function createMenuItem(req: Request, res: Response) {
 
     const item = await prisma.menuItem.create({
       data: {
-        name: 'Chole',
-        price: 100,
-        category: 'Main Course',
-        description: 'Type of curry',
-        available: true,
-        isVeg: true,
-        spiceLevel: 'mild',
-        cookingTime: 15,
-        isPopular: false,
-        allergens: [],
+        name,
+        price,
+        category,
+        description,
+        available,
+        isVeg,
+        spiceLevel,
+        cookingTime,
+        isPopular,
+        allergens,
         calories: 10,
-        protein: 10,
-        carbs: 10,
-        fat: 10,
-        restaurantId: '8870880' // Use a valid Restaurant ID from your database
-      }
+        protein,
+        carbs,
+        fat,
+        restaurantId // Use a valid Restaurant ID from your database
+      } as any
+
     });
     console.log(`Menu item created: ${JSON.stringify(item)}`);
     res.status(201).json(item);
